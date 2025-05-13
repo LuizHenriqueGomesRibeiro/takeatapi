635bbcda81c2109e4a08d65f70d0e36b4b99ea9e
Luiz Gomes <louisnuuk@hotmail.com>
2025-05-13 16:01:36 -0300

    feat

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,15 @@
 import { Sequelize } from 'sequelize';
 import { fileURLToPath } from 'url';
-
-import sequelize from '../config/database';
 import path from 'path';
 import fs from 'fs';
+
+// Importa a configuração do Sequelize
+import sequelize from '../config/database';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Define a interface do db para armazenar os modelos e a instância do Sequelize
 const db: {
     [key: string]: any;
     sequelize: Sequelize;
@@ -17,16 +19,19 @@ const db: {
     Sequelize,
 };
 
+// Carrega todos os arquivos de modelos (exceto o próprio index.ts)
 const files = fs
-  .readdirSync(__dirname)
-  .filter(file => file !== 'index.ts' && file.endsWith('.ts'));
+    .readdirSync(__dirname)
+    .filter(file => file !== 'index.ts' && file.endsWith('.ts'));
 
+// Carrega os modelos dinamicamente usando importação dinâmica (ESM)
 for (const file of files) {
     const model = await import(`./${file}`);
     const modelInstance = model.default(sequelize);
     db[modelInstance.name] = modelInstance;
 }
 
+// Definindo os relacionamentos entre os modelos
 db.Order.belongsTo(db.Product, { foreignKey: 'productId', as: 'product' });
 db.Order.belongsTo(db.Restaurant, { foreignKey: 'restaurantId', as: 'restaurant' });
 db.Order.belongsTo(db.Buyer, { foreignKey: 'buyerId', as: 'buyer' });
