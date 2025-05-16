acc53571e30b8eb02dee4364f54d6a6b10bdf6b2
Luiz Gomes <louisnuuk@hotmail.com>
2025-05-16 18:07:34 -0300

    FEAT

--- dist/config/database/index.js
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from "sequelize";
-const sequelize = new Sequelize('takeat', 'postgres', 'password', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false
-});
-export default sequelize;
