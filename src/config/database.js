bfcc4310cf51a85596eff084878182724c883e42
Luiz Gomes <louisnuuk@hotmail.com>
2025-05-20 15:21:16 -0300

    feat

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
-  username: "postgres",
-  password: "password",
-  database: "takeat",
-  host: "localhost",
-  dialect: "postgres",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
 };
